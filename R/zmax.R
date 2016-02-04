7e0c91b671cd961bacac5b5716153cd245d92bc0
Luke Winslow <lwinslow@usgs.gov>
2016-02-04 14:43:48 -0600

    add bathybase zmax data

---
+++ R/zmax.R
@@ -0,0 +1,19 @@
+#'@title zmax data table 
+#'
+#'
+#'@name zmax
+#'
+#'@return zmax data.frame with all available lake max depth values
+#'
+#'\tabular{lll}{
+#'site_id \tab character \tab Unique identifier of site\cr
+#'zmax_m \tab numeric \tab lake max depth in meters\cr
+#'}
+#'
+#'@docType data
+#'
+#'@examples
+#'head(zmax)
+#'
+#'@export zmax
+NULL
---
+++ data-raw/depth/depths.R
@@ -0,0 +1,22 @@
+#read in zmax data
+
+bathybase = read.csv('data-raw/depth/bathynhd_final.csv', sep=',', header=TRUE, as.is=TRUE)
+bathybase$lat = NULL
+bathybase$lon = NULL
+
+names(bathybase) = c('bathybaseid', 'zmax_m', 'site_id')
+
+zmax = bathybase[, c('site_id', 'zmax_m')]
+
+#Add area data to sysdata if it doesn't already contain it
+if(file.exists('R/sysdata.rda')){
+	sysdata = new.env()
+	load('R/sysdata.rda', envir=sysdata, verbose=TRUE)
+	rm(sysdata, envir=sysdata)#weird hack, I don't understand save
+}else{
+	sysdata = new.env()
+}
+
+sysdata$zmax = zmax
+save(list=names(sysdata), file = "R/sysdata.rda", envr=sysdata, compress=TRUE)
+
