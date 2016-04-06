25822db0220faae1315e9f1f72065594e4613947
Luke Winslow <lwinslow@usgs.gov>
2016-04-06 11:28:47 -0500

    trying to fix this revert

--- R/canopy.R
+++
@@ -1,9 +0,0 @@
-#' @title Canopy heights
-#'
-#' @description Canopy heights in meters for lakes indexed by NHD ID. 
-#'
-#' @docType data
-#' 
-#' @format A data frame with three columns: \code{site_id}, \code{canopy_m},
-#'   and \code{source}.
-"canopy"
