f107d9d5364a083448041b2ba6743f4b811a07c9
Luke Winslow <lwinslow@usgs.gov>
2016-08-24 14:32:16 -0500

    add NHD to WBIC (WI) crosswalk for IDs

---
+++ R/nhd2wbic.R
@@ -0,0 +1,20 @@
+#' @title NHD ID to WBIC (Wisconsin) ID table
+#' 
+#' 
+#' @name nhd2wbic
+#' 
+#' @format nhd2wbic data.frame site_id (NHD) and WBIC columns
+#' 
+#' \tabular{lll}{
+#' Name \tab Type \tab Description\cr
+#' site_id \tab character \tab Unique identifier of site\cr
+#' WBIC \tab numeric \tab Wisconsin Water Body Identifier code \cr
+#' }
+#' 
+#' @docType data
+#' 
+#' @examples
+#' head(nhd2wbic)
+#' 
+#' @export
+NULL
